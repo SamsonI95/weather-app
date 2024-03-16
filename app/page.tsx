0ff39d4177ef2efd40c3ec033c4863cc6b4d00d5
SamsonI95 <samsoniweibo@gmail.com>
2024-03-16 01:52:16 +0100

    Weather app files update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import Navbar from "./Component/Navbar";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { format, parseISO } from "date-fns";
+import Container from "./Component/Container";
+import { convertKelvinToCelcius } from "./utils/convertKelvinToCelcius";
+import { RxDoubleArrowUp } from "react-icons/rx";
+import { RxDoubleArrowDown } from "react-icons/rx";
 
 //Api
 //https://api.openweathermap.org/data/2.5/forecast?q=akure&appid=cc6d71e6cc19bbed0694bcae70b59d86
@@ -58,9 +67,77 @@ type WeatherData = {
 };
 
 export default function Home() {
+  const { isPending, error, data } = useQuery<WeatherData>({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=akure&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      );
+      return data;
+    },
+  });
+
+  const firstData = data?.list[0];
+
+  console.log("data", data?.city);
+
+  if (isPending)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/*Today data*/}
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-center">
+              <p className="text-2xl">
+                {" "}
+                {format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}{" "}
+              </p>
+              <p className="text-lg">
+                {" "}
+                {format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")}{" "}
+              </p>
+            </h2>
+            <Container className="gap-10 px-6 items-center">
+              {/*Temperature*/}
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToCelcius(firstData?.main.temp ?? 296.37)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>
+                    {convertKelvinToCelcius(firstData?.main.temp ?? 0)}°
+                  </span>
+                </p>
+                <p className="text-xs space-x-2 flex items-center">
+                <span className="flex items-center">
+                    {convertKelvinToCelcius(firstData?.main.temp_min ?? 0)}°
+                    <RxDoubleArrowDown />
+                  </span>
+                  <span className="flex items-center">
+                    {convertKelvinToCelcius(firstData?.main.temp_max ?? 0)}°
+                    <RxDoubleArrowUp />
+                  </span>
+                </p>
+              </div>
+
+              {/*Time and weather icon*/}
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between"></div>
+            </Container>
+          </div>
+        </section>
+
+        {/*7 day forecast data*/}
+        <section></section>
+      </main>
     </div>
   );
 }
---
+++ app/utils/convertKelvinToCelcius.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+export function convertKelvinToCelcius(tempInKelvin: number): number {
+    const tempInCelcius = tempInKelvin - 273.15;
+    return Math.floor(tempInCelcius);
+}
